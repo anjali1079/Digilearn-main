410d3ba6e21141ba266e56b0af57e72e4c5b46df
anjali1079 <anjalijogale123@gmail.com>
2025-04-13 19:18:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  build: {
+    rollupOptions: {
+      external: ['netlify-identity-widget']
+    }
+  }
 });
+
