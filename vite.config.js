6cd55bba9307ba92f9dc67e185798513333be1b2
anjali1079 <anjalijogale123@gmail.com>
2025-04-13 19:25:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['netlify-identity-widget']
-    }
-  }
+      external: ['netlify-identity-widget'], // Mark as external
+    },
+  },
 });
